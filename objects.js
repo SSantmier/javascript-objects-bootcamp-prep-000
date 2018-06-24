0e4754fd6bdc8f06d9818ebbb82ae31919232da0
Sara S <ssantmier@gmail.com>
2018-06-24 20:05:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist = { Slowdive: "Alison", My Bloody Valentine: "Sometimes"}
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
   
-function updatePlaylist()
+function updatePlaylist() =
   
 function removeFromPlaylist()
