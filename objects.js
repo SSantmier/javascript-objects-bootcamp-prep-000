ff5b3e30e9db9b068e7f4013332cb9a3d6f61093
Sara S <ssantmier@gmail.com>
2018-06-24 20:10:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
   
 function updatePlaylist() {playlist['Phil Ochs']= "Here's to the State of Mississippi"}
   
-function removeFromPlaylist() {
+function removeFromPlaylist() {delete playlist.slowdive; playlist}
